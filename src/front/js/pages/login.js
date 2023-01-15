2833c28f4bcf8dcc94488426960a28e8f9fe80e7
faldea1 <faldea1@uc.cl>
2023-01-15 16:01:29 +0000

    Token stored

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -2,45 +2,34 @@ import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
 	const { store, actions } = useContext(Context);
     // local States: para poder tener email/pass en una variable y enviar a backend. Componente controlado.
     const [ email, setEmail ] = useState("");
     const [ password, setPassword ] = useState("");
+    const navigate = useNavigate();
+
+  
+    console.log('this is your token', store.token);
 
     const handleClick = () => {
+        actions.login(email, password);
+    };
 
-        const options = {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                "email": email,
-                "password": password
-            })
-        }
-
-        fetch('https://3001-4geeksacade-reactflaskh-j7dwm0jofz6.ws-us82.gitpod.io/api/token', options)
-            .then(resp => {
-                if(resp.status === 200) return resp.json();
-                else alert ('there has been some problem issues');
-            })
-            .then()
-            .catch(error => {
-                console.error('We have a problem', error)
-            })
-    }
+    if(store.token && store.token != "" && store.token != undefined) navigate("/");
 
 	return (
 		<div className="text-center mt-5">
 			<h1>Login</h1>
-			<div>
-                <input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button onClick={handleClick}>Login</button>
-            </div>
+                {(store.token && store.token != "" && store.token != undefined) ? "You are logged in with this token!" + store.token :
+                <div>   
+                    <input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <button onClick={handleClick}>Login</button>
+                </div>
+                }
 		</div>
 	);
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			token: null,
 			message: null,
 			demo: [
 				{
@@ -20,6 +21,51 @@ const getState = ({ getStore, getActions, setStore }) => {
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
+
+			syncTokenFromLocalStore: () => {
+				const token = localStorage.getItem("token");
+				console.log('aplication loaded, synching the local storage token');
+				if(token && token !="" && token != undefined) setStore({ token: token });
+			},
+
+			logout: () => {
+				localStorage.removeItem("token");
+				console.log('Log off');
+				setStore({ token: null });
+			},
+
+			//Realizar fetch aquí para que sea una variable global (para que toda la aplicación tenga acceso al token almacenado).
+			login: async (email, password) => {
+				const options = {
+					method: 'POST',
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						"email": email,
+						"password": password
+					})
+				};
+
+				try {
+					const resp = await fetch('https://3001-4geeksacade-reactflaskh-j7dwm0jofz6.ws-us82.gitpod.io/api/token', options)
+					if (resp.status !== 200){
+						alert ('there has been some problem issues');
+						return false;
+					} 
+					
+					const data = await resp.json();
+					console.log('from the backend', data);		
+					//localStorage o sessionStorage: guardar token y no perderlo al "actualizar". Guardar variable x con valor x.					
+					localStorage.setItem("token", data.access_token);
+					setStore({ token: data.access_token });
+					return true;
+				}
+				catch(error){
+					console.error('there has been an error trying login in')
+				}
+			},
+
 
 			getMessage: async () => {
 				try{
